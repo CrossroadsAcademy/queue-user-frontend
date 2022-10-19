a9ed8ccb121de536effb49a726fed5a5591f519e
JiyadAhammad <jiyadahammad99@gmail.com>
2022-10-19 22:18:36 +0530

    Lint rule added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:queue/Login/SignUp/create_account.dart';
+
+import 'presentation/splash/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,25 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return const SafeArea(
-      child: CreateAccount(),
+      home: const SplashScreen(),
     );
   }
 }
