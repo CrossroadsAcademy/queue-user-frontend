4c4507e5e99dffc299234e68783b40c07fe4ad56
Abin56 <abinjohn8089@gmail.com>
2022-10-31 14:21:30 +0530

    Introduction Ui screen Complete and add app main elevated button widget

---
+++ lib/presentation/introduction_screen/introduction_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../constant/colors/colors.dart';
+import '../widgets/elevated_button_widget/elevated_button.dart';
+
+class IntroductionScreen extends StatelessWidget {
+  const IntroductionScreen({super.key});
+
+  static const String routeName = '/introduction';
+  static Route<IntroductionScreen> route() {
+    return MaterialPageRoute<IntroductionScreen>(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const IntroductionScreen(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: appBgColor,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset('assets/images/queueIntro.png'),
+              ElvatedButtonWidget(
+                buttonText: 'Get Started',
+                onpressed: () {
+                  Navigator.pushReplacementNamed(context, '/login');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
